42a3a965ca0ff904aebbd2fdfba816d65a29c5a8
DwTzuyu <deweiche@ucm.es>
2022-04-04 00:53:20 +0200

    add gamepage and some logic

--- TrabajoDSI2022G10/GamePage.xaml.cs
+++ TrabajoDSI2022G10/GamePage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +23,32 @@ namespace TrabajoDSI2022G10
     /// </summary>
     public sealed partial class GamePage : Page
     {
+        Button sel;
         public GamePage()
         {
             this.InitializeComponent();
+            
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sel = sender as Button;
+            MenuFlyout flyout = new MenuFlyout();
+            MenuFlyoutItem item = new MenuFlyoutItem();
+            item.Text = "Off";
+            var bmi = new BitmapIcon();
+            bmi.UriSource = new Uri("ms-appx:///Assets/Boton_Quit.png");
+            bmi.ShowAsMonochrome = false;
+            item.Icon = bmi;
+            item.Click += (s, e1) =>
+            {
+                Image img = new Image();
+                img.Source = new BitmapImage(new Uri("ms-appx:///Assets/Boton_Quit.png"));
+                sel.Content = img;
+
+            };
+
+            flyout.Items.Add(item);
+            sel.Flyout = flyout;
+        }    
     }
 }
