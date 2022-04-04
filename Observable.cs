7bd5673ac6d0c3f4623d03c56e14ede34e9a3f58
DwTzuyu <deweiche@ucm.es>
2022-04-04 20:52:12 +0200

    add buy win,lose

---
+++ Observable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class Class1 : INotifyPropertyChanged
+{
+	public Class1()
+	{
+	}
+	protected void RaisePropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+	public event PropertyChangedEventHandler PropertyChanged;
+	RaisePropertyChanged(nameof(MyProperty));
+    protected bool Set<T>(
+    ref T field,
+    T newValue,
+    [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, newValue))
+        {
+            return false;
+        }
+
+        field = newValue;
+        RaisePropertyChanged(propertyName);
+        return true;
+    }
+}
--- TrabajoDSI2022G10/GamePage.xaml.cs
+++ TrabajoDSI2022G10/GamePage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Media.Imaging;
+using System.ComponentModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,14 +22,41 @@ namespace TrabajoDSI2022G10
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GamePage : Page
+    public sealed partial class GamePage : Page, INotifyPropertyChanged
     {
         Button sel;
+        Button object2buy;
+        //int ice=0, bomb=0, shield, vaccine, wind;
+        //private DispatcherTimer _btimer,_itimer,_stimer,_vtimer,_wtimer;
         public GamePage()
         {
             this.InitializeComponent();
-            
+            //_btimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            //_itimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            //_stimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            //_vtimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            //_wtimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+
+            //_btimer.Tick += (sender, o) =>
+            //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(bomb)));
+            //_itimer.Tick += (sender, o) =>
+            //   PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ice)));
+            //_stimer.Tick += (sender, o) =>
+            //   PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(shield)));
+            //_vtimer.Tick += (sender, o) =>
+            //   PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(vaccine)));
+            //_wtimer.Tick += (sender, o) =>
+            //   PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(wind)));
+
+            //_btimer.Start();
+            //_itimer.Start();
+            //_stimer.Start();
+            //_vtimer.Start();
+            //_wtimer.Start();
+
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             sel = sender as Button;
@@ -63,26 +91,78 @@ namespace TrabajoDSI2022G10
         }
         private void PauseB_Click(object sender, RoutedEventArgs e)
         {
-
+            PausePanel.Visibility = Visibility.Visible;
         }
         private void Objects_Click(object sender, RoutedEventArgs e)
         {
-
+            object2buy = sender as Button;
+            switch (object2buy.Name)
+            {
+                case "Bomb":
+                    Ctxt.Text = "QUIERES GASTAR 150 MONEDAS PARA COMPRAR BOMBA";
+                    break;
+                case "Ice":
+                    Ctxt.Text = "QUIERES GASTAR 100 MONEDAS PARA COMPRAR HIELO";
+                    break;
+                case "Vaccine":
+                    Ctxt.Text = "QUIERES GASTAR 100 MONEDAS PARA COMPRAR VACUNA";
+                    break;
+                case "Wind":
+                    Ctxt.Text = "QUIERES GASTAR 80 MONEDAS PARA COMPRAR VIENTO";
+                    break;
+                case "Shield":
+                    Ctxt.Text = "QUIERES GASTAR 120 MONEDAS PARA COMPRAR ESCUDO";
+                    break;
+            }
+            BuyPanel.Visibility = Visibility.Visible;
         }
 
         private void MenuButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Frame.Navigate(typeof(MainPage));
         }
 
         private void AgainButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PausePanel.Visibility = Visibility.Collapsed;
         }
 
         private void ContinueButton_Click(object sender, RoutedEventArgs e)
         {
+            PausePanel.Visibility = Visibility.Collapsed;
+        }
 
+        private void BuyBack_Click(object sender, RoutedEventArgs e)
+        {
+            BuyPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void Buy_Click(object sender, RoutedEventArgs e)
+        {
+            BuyPanel.Visibility = Visibility.Collapsed;
+            switch (object2buy.Name)
+            {
+                case "Bomb":
+                    
+                    break;
+                case "Ice":
+                    
+                    break;
+                case "Vaccine":
+                    
+                    break;
+                case "Wind":
+                    
+                    break;
+                case "Shield":
+                    
+                    break;
+            }
+        }
+
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NivelPage));
         }
     }
 }
